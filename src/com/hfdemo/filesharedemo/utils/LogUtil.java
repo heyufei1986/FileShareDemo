4abc833e6fd84828b91640ac9e7b99b7c47d05d1
feihe <heyufei1986@qq.com>
2014-08-28 17:31:29 +0800

    add　log utils file

---
+++ src/com/hfdemo/filesharedemo/utils/LogUtil.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.hfdemo.filesharedemo.utils;
+
+import android.util.Log;
+
+//import com.tencent.mtt.R;
+
+/*
+ * Copyright (C) 2005-2010 TENCENT Inc.All Rights Reserved.
+ * FileName：StringUtil.java
+ * Description：
+ * History：
+ * 1.0 theonewang 2011-12-19 Create
+ */
+
+public class LogUtil
+{
+	public static void i(String tag, String msg)
+	{
+		Log.i(tag, msg);
+	}
+}
